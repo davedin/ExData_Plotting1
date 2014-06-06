f4e270e2439f6c95ed210d58cd7af617e2ac19ef
David DiNatale <david.dinatale@philips.com>
2014-06-06 02:09:59 -0400

    plot1 done - includes R script and PNG output

---
+++ plot1.R
@@ -0,0 +1,45 @@
+# setwd('C:/www/r/exdata-project1')
+
+# download, unzip, and read file
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.table( unz( temp, "household_power_consumption.txt" )
+                   , nrows = 100000
+                   , sep = ";"
+                   #   , colClasses = c("POSIXct", "POSIXct","numeric")
+                   , header = TRUE
+)
+unlink(temp)
+
+
+# create a datetime field
+data$DateTime <- strptime(paste(data$Date, data$Time),format="%d/%m/%Y %H:%M:%S")
+
+
+# get the relevant data only
+relevant <- data[grep("2007-02-0[1|2]",data$DateTime,]
+head(relevant)
+tail(relevant)
+
+
+# open a PNG device
+png(file = "plot1.png", bg = "transparent", width = 480, height = 480)
+
+
+# create the histogram
+    hist( as.numeric( relevant$Global_active_power ) / 550
+          , breaks = 12
+          , col = "red" 
+          , main = "Global Active Power"
+          , xlab = "Global Active Power (kilowatts)"
+          , ylab = "Frequency"
+          , xlim=c(0,6)
+          , ylim=c(0,1200)
+          , axes = FALSE
+          )
+    
+    axis(1, seq( by= 2,from = 0, to = 6 ) )
+    axis(2, seq( by= 200,from = 0, to = 1200 ) )
+
+# close the PNG device
+dev.off()
