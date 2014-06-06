a218542ece166ee5d36408e88b3822592bc29850
David DiNatale <david.dinatale@philips.com>
2014-06-06 12:24:42 -0400

    update plot1 with better numeric data

--- plot1.R
+++ plot1.R
@@ -1,15 +1,25 @@
 # setwd('C:/www/r/exdata-project1')
 
 # download, unzip, and read file
-temp <- tempfile()
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-data <- read.table( unz( temp, "household_power_consumption.txt" )
-                   , nrows = 100000
-                   , sep = ";"
-                   #   , colClasses = c("POSIXct", "POSIXct","numeric")
-                   , header = TRUE
-)
-unlink(temp)
+if( !file.exists("household_power_consumption.txt") ) { # go get the file
+    
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","household_power_consumption.zip")
+    unzip( "household_power_consumption.zip")
+    data <- read.table( "household_power_consumption.txt"
+                        , nrows = 100000
+                        , sep = ";"
+                        , header = TRUE
+    )
+    
+} else { # use your local file
+    
+    data <- read.table( "household_power_consumption.txt"
+                        , nrows = 100000
+                        , sep = ";"
+                        , header = TRUE
+    )
+    
+}
 
 
 # create a datetime field
@@ -27,19 +37,19 @@ png(file = "plot1.png", bg = "transparent", width = 480, height = 480)
 
 
 # create the histogram
-    hist( as.numeric( relevant$Global_active_power ) / 550
-          , breaks = 12
+    hist( as.numeric( as.character(relevant$Global_active_power ) )
+         # , breaks = 12
           , col = "red" 
           , main = "Global Active Power"
           , xlab = "Global Active Power (kilowatts)"
           , ylab = "Frequency"
-          , xlim=c(0,6)
-          , ylim=c(0,1200)
-          , axes = FALSE
+          #, xlim=c(0,6)
+          #, ylim=c(0,1200)
+          #, axes = FALSE
           )
     
-    axis(1, seq( by= 2,from = 0, to = 6 ) )
-    axis(2, seq( by= 200,from = 0, to = 1200 ) )
+#    axis(1, seq( by= 2,from = 0, to = 6 ) )
+#    axis(2, seq( by= 100,from = 0, to = 1200 ) )
 
 # close the PNG device
 dev.off()
